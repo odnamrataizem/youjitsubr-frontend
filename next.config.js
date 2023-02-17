01dca17ec2430d998de1538970cdb5f8b8c2362a
Armando Meziat <amf-commits@mezi.at>
2023-02-17 00:05:59 -0300

    Use Preact in production

--- next.config.js
+++ next.config.js
@@ -19,6 +19,17 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat',
+      });
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
