6f9f2e8a4a83bd4946a939023bcefd9dcb747ebf
Armando Meziat <amf-commits@mezi.at>
2023-02-22 13:08:18 -0300

    Use another way to set inline script contents

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,18 +2,24 @@ import { Html, Head, Main, NextScript } from 'next/document';
 import Script from 'next/script';
 
 export default function Document() {
+  const __html = `
+    var colorScheme = localStorage ? localStorage.theme : null;
+    var media = matchMedia('(prefers-color-scheme: dark)');
+    colorScheme = colorScheme || (media && media.matches ? 'dark' : 'light');
+    document.documentElement.classList.toggle('dark', colorScheme === 'dark');
+  `;
+
   return (
     <Html lang={process.env.NEXT_PUBLIC_LOCALE}>
       <Head />
       <body>
         <Main />
         <NextScript />
-        <Script id="initial-theme" strategy="beforeInteractive">{`
-          const colorScheme =
-            localStorage?.theme ??
-            (matchMedia('(prefers-color-scheme: dark)')?.matches ? 'dark' : 'light');
-          document.documentElement.classList.toggle('dark', colorScheme === 'dark');
-        `}</Script>
+        <Script
+          id="initial-theme"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{ __html }}
+        ></Script>
       </body>
     </Html>
   );
