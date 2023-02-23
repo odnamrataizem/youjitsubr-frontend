120eafe9a542d8b83f750b1857bff53929e1993d
Armando Meziat <amf-commits@mezi.at>
2023-02-22 23:38:22 -0300

    Add author profile to post footer

--- src/components/SinglePost.tsx
+++ src/components/SinglePost.tsx
@@ -18,7 +18,7 @@ export default function SinglePost({ data }: SinglePostProps) {
 
   return (
     data && (
-      <article>
+      <StyledArticle>
         <StyledHeader>
           <ProseContainer>
             <StyledTitle>{data.title}</StyledTitle>
@@ -94,26 +94,54 @@ export default function SinglePost({ data }: SinglePostProps) {
         </ProseContainer>
         <StyledFooter>
           <ProseContainer>
-            <section>
+            <StyledTagsSection>
               <h2 className={visuallyHidden}>Tags</h2>
-              <StyledTags>
+              <ul>
                 {data.tags.map(tag => (
                   <li key={tag.id}>
                     <EntityLink kind="tags" data={tag} />
                   </li>
                 ))}
-              </StyledTags>
+              </ul>
+            </StyledTagsSection>
+            <section>
+              <h2 className={visuallyHidden}>Autores</h2>
+              {data.authors.map(author => (
+                <StyledAboutSection key={author.id}>
+                  <StyledAuthorPictureContainer>
+                    <Image fill src={author.picture?.url ?? ''} alt="" />
+                  </StyledAuthorPictureContainer>
+                  <StyledAuthorInfoContainer>
+                    <h3>{author.name}</h3>
+                    <div>
+                      <Renderer document={author.description.document} />
+                    </div>
+                  </StyledAuthorInfoContainer>
+                  <StyledLinkContainer>
+                    <EntityLink kind="people" data={author}>
+                      Ver posts
+                      <span className={visuallyHidden}> de {author.name}</span>
+                    </EntityLink>
+                  </StyledLinkContainer>
+                </StyledAboutSection>
+              ))}
             </section>
           </ProseContainer>
         </StyledFooter>
-      </article>
+      </StyledArticle>
     )
   );
 }
 
-const StyledHeader = styled.header`
-  padding-block-start: var(--size-12);
+const StyledArticle = styled.article`
+  padding-block: var(--size-12);
 
+  @media (width < ${Breakpoints.MD}) {
+    padding-block: var(--size-6);
+  }
+`;
+
+const StyledHeader = styled.header`
   h1,
   p {
     margin: 0;
@@ -121,10 +149,6 @@ const StyledHeader = styled.header`
 
   a {
     font-weight: var(--weight-semibold);
-  }
-
-  @media (width < ${Breakpoints.MD}) {
-    padding-block-start: var(--size-6);
   }
 `;
 
@@ -143,19 +167,14 @@ const StyledMeta = styled.p`
 
 const StyledFooter = styled.footer`
   font-size: var(--scale-3);
+`;
 
+const StyledTagsSection = styled.section`
   ul {
     margin: 0;
     padding: 0;
   }
 
-  a {
-    font-weight: var(--weight-black);
-    text-decoration: none;
-  }
-`;
-
-const StyledTags = styled.ul`
   li {
     display: inline-block;
 
@@ -164,10 +183,97 @@ const StyledTags = styled.ul`
     }
 
     a {
+      font-weight: var(--weight-black);
+      text-decoration: none;
+
       &::before {
         content: '#';
         opacity: 0.75;
       }
     }
+  }
+`;
+
+const StyledAboutSection = styled.section`
+  background: var(--color-gray-100);
+  display: flex;
+  flex-wrap: wrap;
+  position: relative;
+  border-radius: var(--radius-sm);
+  margin-block: var(--scale-4);
+  padding: var(--size-6);
+  gap: var(--size-6);
+  align-items: center;
+  font-size: var(--scale-0);
+
+  h3 {
+    margin-block-start: 0;
+
+    + div :last-child {
+      margin-block-end: 0;
+    }
+
+    @media (width < ${Breakpoints.MD}) {
+      + div {
+        margin-inline-start: calc(var(--size-20) * -1 + var(--size-6) * -1);
+      }
+    }
+  }
+
+  html.dark & {
+    background: var(--color-gray-800);
+  }
+
+  @media (width >= ${Breakpoints.MD}) {
+    border-radius: var(--radius-lg);
+    margin-block: var(--scale-4);
+    padding: var(--size-8);
+    gap: var(--size-8);
+  }
+`;
+
+const StyledAuthorPictureContainer = styled.div`
+  position: relative;
+  inline-size: var(--size-20);
+  block-size: var(--size-20);
+  align-self: flex-start;
+  margin-block-start: calc(var(--size-5) * -1);
+
+  img {
+    object-fit: cover;
+    border-radius: var(--radius-full);
+  }
+
+  @media (width >= ${Breakpoints.MD}) {
+    inline-size: var(--size-36);
+    block-size: var(--size-36);
+    margin-block-start: 0;
+    align-self: unset;
+  }
+`;
+
+const StyledAuthorInfoContainer = styled.div`
+  flex: 1;
+`;
+
+const StyledLinkContainer = styled.small`
+  align-self: flex-start;
+
+  a {
+    text-decoration: none;
+    color: var(--color-gray-50);
+    border: var(--size-px) solid var(--color-youjitsu-1);
+    border-radius: var(--radius-xs);
+    padding-inline: var(--size-3);
+    padding-block: var(--size-2);
+
+    html.dark & {
+      color: var(--color-gray-50);
+      border-color: var(--color-youjitsu-2);
+    }
+  }
+
+  @media (width > ${Breakpoints.MD}) {
+    inset-inline-end: var(--size-8);
   }
 `;
