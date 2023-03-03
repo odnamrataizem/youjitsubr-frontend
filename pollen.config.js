4f06e78097145d81bb273c453d0fdb3c1b23e57e
Armando Meziat <amf-commits@mezi.at>
2023-03-03 16:23:03 -0300

    Add styling to paginator

--- pollen.config.js
+++ pollen.config.js
@@ -7,6 +7,10 @@ const config = pollen => ({
       'youjitsu-1': '#a4003e',
       'youjitsu-2': '#e5025a',
     },
+    size: {
+      ...pollen.size,
+      '2px': '2px',
+    },
     scaleFluid: false,
     width: false,
     line: false,
--- src/components/Paginator.tsx
+++ src/components/Paginator.tsx
@@ -1,9 +1,12 @@
+import { styled } from '@linaria/react';
 import usePagination from '@mui/material/usePagination';
+import { useMediaQuery } from '@react-hook/media-query';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import React, { useCallback } from 'react';
 
 import { PAGE_PREFIX } from '../lib/routing';
+import { Breakpoints } from '../styles/common';
 
 type PaginatorProps = {
   page: number;
@@ -11,15 +14,17 @@ type PaginatorProps = {
 };
 
 export default function Paginator({ page, lastPage }: PaginatorProps) {
+  const isPhone = useMediaQuery(`(width < ${Breakpoints.SM})`);
+
   const { items } = usePagination({
-    boundaryCount: 2,
+    boundaryCount: isPhone ? 1 : 2,
     page,
     count: lastPage,
     hideNextButton: true,
     hidePrevButton: true,
     showFirstButton: false,
     showLastButton: false,
-    siblingCount: 2,
+    siblingCount: isPhone ? 1 : 2,
   });
 
   const { asPath } = useRouter();
@@ -35,11 +40,11 @@ export default function Paginator({ page, lastPage }: PaginatorProps) {
 
   return (
     <nav>
-      <ul style={{ display: 'flex', gap: '5px' }}>
+      <StyledPaginator>
         {items.map(({ page: thisPage, type, selected, ...item }, index) => (
-          <li key={index} style={{ display: 'block' }}>
+          <li key={index}>
             {type.endsWith('-ellipsis') ? (
-              '…'
+              <div>…</div>
             ) : page === thisPage ? (
               <span>{thisPage}</span>
             ) : (
@@ -49,7 +54,66 @@ export default function Paginator({ page, lastPage }: PaginatorProps) {
             )}
           </li>
         ))}
-      </ul>
+      </StyledPaginator>
     </nav>
   );
 }
+
+const StyledPaginator = styled.ul`
+  display: flex;
+  justify-content: center;
+  gap: var(--size-4);
+  flex-wrap: wrap;
+
+  li {
+    display: block;
+
+    > * {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      min-inline-size: var(--size-11);
+      block-size: var(--size-11);
+      font-size: var(--scale-0);
+      font-weight: var(--weight-medium);
+      border-radius: var(--radius-sm);
+      border: var(--size-2px) solid var(--color-youjitsu-1);
+      color: var(--color-youjitsu-1);
+      padding-inline: var(--size-1);
+
+      html.dark & {
+        border-color: var(--color-youjitsu-2);
+        color: var(--color-youjitsu-2);
+      }
+
+      @media (width < ${Breakpoints.MD}) {
+        min-inline-size: var(--size-9);
+        block-size: var(--size-9);
+        font-size: var(--scale-00);
+      }
+    }
+
+    > div {
+      border: 0;
+    }
+
+    > span {
+      background-color: var(--color-youjitsu-1);
+      color: var(--color-grey-50);
+
+      html.dark & {
+        background: var(--color-youjitsu-2);
+        color: var(--color-grey-50);
+      }
+    }
+
+    > a {
+      text-decoration: none;
+    }
+  }
+
+  @media (width < ${Breakpoints.SM}) {
+    gap: var(--size-2);
+    margin-inline: calc(var(--size-5) * -1);
+  }
+`;
